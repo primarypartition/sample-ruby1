ba7d10890eb95f8e392c3f38b8d51bc3e754b5ff
vagrant <vagrant@master>
2020-05-01 19:56:37 +0000

    project init

---
+++ basic1/var.rb
@@ -0,0 +1,31 @@
+#add = [1,2,3,4,5]
+
+#p add.reverse!
+
+# puts "hello ali!"
+
+#print "Hello Ali again"
+
+#my_name = "ali"
+
+#p my_name
+
+# def my_method(arg)
+
+#   puts "my method"  
+#   puts arg
+
+# end
+
+# my_method("ali")
+
+
+# def my_method2(val1, val2)
+
+#   return val1 + val2
+
+# end
+
+
+# print my_method2(1, 2)
+
